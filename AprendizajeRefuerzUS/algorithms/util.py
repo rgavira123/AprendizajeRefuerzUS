a7f316e7b459ade5a793877213c85522346ba4fe
Daniel Ruiz Lopez <danielruizlopezcc@gmail.com>
2024-06-10 08:37:34 +0200

    Clase util comentada

--- AprendizajeRefuerzUS/algorithms/util.py
+++ AprendizajeRefuerzUS/algorithms/util.py
@@ -3,6 +3,19 @@ import math as math
 
 
 def calcula_dimensiones(transiciones):
+    """
+    Calcula las dimensiones de la matriz de transiciones
+
+    Parametros
+    ----------
+
+    transiciones : Array
+        Matriz de transiciones
+    
+    Si la matriz de transiciones es de dimension 3, se asume que la primera dimension es el numero de acciones
+    y la segunda dimension es el numero de estados.
+    Si la matriz de transiciones es de dimension 2, se asume que la primera dimension es el numero de estados
+    """
     acciones = len(transiciones)
     if transiciones.ndim==3:
         estados = transiciones.shape[1]
@@ -11,12 +24,33 @@ def calcula_dimensiones(transiciones):
     return estados, acciones
 
 def politica_procesable(politica,acciones):
+    """
+    Comprueba si la politica es procesable
+
+    Parametros
+    ----------
+    politica : Array
+        Array con la politica
+
+    acciones : Array
+        Array con las acciones posibles
+    """
     if isinstance(politica, dict):
         return [acciones[i] for i in politica.values()]
     else:
         return [acciones[i] for i in politica]
     
 def obtener_politica_final(politica):
+    """
+    Obtiene la politica final
+
+    Parametros
+    ----------
+
+    politica : Array
+        Array con la politica
+    """
+    
     indices_nav_acciones = {'esperar': 0, 'N': 1, 'NE': 2, 'E': 3, 'SE': 4, 'S': 5, 'SO': 6, 'O': 7, 'NO': 8}
     politica_greedy_indices = [indices_nav_acciones[a] for a in politica]
     politica = enumerate(politica_greedy_indices)
